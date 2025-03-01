30be037a4a22365f6f29e70fb9c5830065556c89
Chaitanya <rajivchaitanya143@gmail.com>
2025-03-01 21:05:51 +0530

    ColorPicker

---
+++ colorPicker.js
@@ -0,0 +1,22 @@
+let ContainerColor=document.getElementById("colorPickerContainer");
+let hexCode=document.getElementById("spancolor");
+function colorOne(){
+    ContainerColor.style.backgroundColor="#e0e0e0";
+    hexCode.textContent="#e0e0e0";
+    hexCode.style.color="#49a6e9";
+}
+function colorTwo(){
+    ContainerColor.style.backgroundColor="#6fcf97";
+    hexCode.textContent="#6fcf97";
+    hexCode.style.color="#49a6e9";
+}
+function colorThree(){
+    ContainerColor.style.backgroundColor="#56ccf2";
+    hexCode.textContent="#56ccf2";
+    hexCode.style.color="#49a6e9";
+}
+function colorFour(){
+    ContainerColor.style.backgroundColor="#bb6bd9";
+    hexCode.textContent="#bb6bd9";
+    hexCode.style.color="#49a6e9";
+}
